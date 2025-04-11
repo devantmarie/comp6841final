947452a29b3f92015d4c268fb54a63039d389668
emilio <emilio.vello@gmail.com>
2025-04-11 16:51:23 -0400

    comments and results 3

--- datasetsnc/dataclasses.py
+++ datasetsnc/dataclasses.py
@@ -8,11 +8,18 @@ from torch.utils.data import Dataset, Subset
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 
-# Classes to load the data. 
-# ******************************
-# here is the class to read the data. biopython to read and then transform?
+"""
+Classes to load and preprocess biological sequence data.
+********************************************************
+
+Utilities and dataset class for loading and preprocessing non-coding RNA (ncRNA) sequences from FASTA files. Includes functions for reading data (requires Biopython), mapping labels, converting sequences to one-hot encoding, applying reverse complement transformations, and visualizing class and length distributions. The `NcRnaDataset` class prepares data for training by resizing sequences and optionally combining multiple FASTA sources.
+"""
+#**************************************************
 
 def read_fasta_file(ffile):
+    """
+    Reads a FASTA file using Biopython and converts it into a pandas DataFrame (label extracted from the sequence description).
+    """
     df = pd.DataFrame(columns = ["id","sequence","ylabel"])
     for record in SeqIO.parse(ffile,"fasta"):
          df.loc[len(df)] = [record.id,record.seq,record.description.split()[-1]]
@@ -24,8 +31,13 @@ def list_y_classes_from_data(df):
     return df["ylabel"].unique() 
 
 #***************************************************    
-# map the rnalabel to number
+
 def ncrna_map():
+    """
+    Returns two dictionaries for ncRNA class mapping:
+    - nc_map: maps ncRNA class names to numeric labels.
+    - nc_map_rev: reverse mapping from numeric labels back to class names.
+    """
     nc_map= {'unknown':0,
              '5S_rRNA':1, 
              '5_8S_rRNA':2, 
@@ -43,11 +55,17 @@ def ncrna_map():
     
     nc_map_rev = {v: k for k, v in nc_map.items()}
     return nc_map,nc_map_rev
-#***************************************************
-# Converts an RNA sequence string to one-hot encoding. Invalid nucleotides (e.g., N, Y, R, etc.) 
-# are encoded as [0, 0, 0, 0].
 
+#***************************************************
+#
 def sequence_to_onehot(sequence):
+    """
+    Converts an RNA sequence into a one-hot encoded tensor.
+        Each nucleotide (A, T, G, C) is encoded into a 4-element binary vector.
+        Invalid or ambiguous bases (e.g., N, Y, R) are encoded as [0, 0, 0, 0].
+    Returns:
+        torch.Tensor: One-hot encoded representation of the input sequence.
+    """
     ncl_index =  {'A':0,'T':1,'G':2,'C':3}  
     sequence_indx = [ncl_index.get(i, -1) for i in sequence]
     onehot = []
@@ -59,8 +77,15 @@ def sequence_to_onehot(sequence):
     return torch.tensor(onehot)
     
 #***************************************************
-#Histogram of Non-Coding RNA by Class
+#
 def hist_plot_ncrna_class(df):
+        """
+        Displays a histogram of ncRNA class distribution from the given DataFrame.
+        Parameters:
+            df (pandas.DataFrame): DataFrame containing a 'ylabel' column with ncRNA class labels.
+        Returns:
+            matplotlib.pyplot: The plot object for further customization if needed.
+        """
     class_counts = df['ylabel'].value_counts()
     plt.figure(figsize=(10, 6))
     class_counts.plot(kind='bar', color='skyblue', edgecolor='black')
@@ -71,9 +96,19 @@ def hist_plot_ncrna_class(df):
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.show()
     return plt
+
 #***************************************************
-# Distribution of Sequence Lengths
+# 
 def dist_seq_lenghts(df):
+    """
+    Displays a histogram showing the distribution of ncRNA sequence lengths.
+    
+    Parameters:
+        df (pandas.DataFrame): DataFrame containing a 'real_sequence_length' column.
+    
+    Returns:
+        matplotlib.pyplot: The plot object for further customization if needed.
+    """
     plt.figure(figsize=(10, 6))
     plt.hist(df['real_sequence_length'], bins=30, color='lightcoral', edgecolor='black', alpha=0.7)
     plt.xlabel("Sequence Length")
@@ -84,8 +119,17 @@ def dist_seq_lenghts(df):
     return plt
 
 #***************************************************
-
+# 
 def reverse_complement(rna_sequence):
+    """
+    Compute the reverse complement of an RNA/DNA sequence.
+    This function handles both standard and ambiguous nucleotide codes.
+    It maps each base to its complement and reverses the sequence.
+    Parameters:
+        rna_sequence (str): The nucleotide sequence (RNA or DNA) as a string.
+    Returns:
+        str: The reverse complement of the input sequence.
+    """
     complement_base= {
         'A': 'T',
         'T': 'A',
@@ -110,9 +154,27 @@ def reverse_complement(rna_sequence):
     return reverse_complement
 
 #*************************************************
-
+# 
 class NcRnaDataset(Dataset):
+    """
+    Custom Dataset class for loading, processing, and transforming non-coding RNA (ncRNA) sequences from FASTA files. 
+    It extends the PyTorch `Dataset` class to be used in a `DataLoader`. The class supports reverse complement transformations, 
+    one-hot encoding of sequences, and resizing sequences to a fixed length. It also includes methods for plotting histograms 
+    of class distributions and sequence lengths.
+    """
+        
     def __init__(self,fastaFile,seq_length=120,random_rev_compl_transform_prob = 0,fastaFile2=None, fastaFile3=None):
+        """
+        Initializes the NcRnaDataset class by loading ncRNA sequences from one or more FASTA files, mapping labels to numerical values, 
+        and computing sequence lengths. It also supports sequence resizing and random reverse complement transformations.
+        
+        Parameters:
+        - fastaFile (str): Path to the primary FASTA file containing the ncRNA sequences.
+        - seq_length (int, optional): The fixed length to which sequences will be resized. Default is 120.
+        - random_rev_compl_transform_prob (float, optional): The probability of applying a random reverse complement transformation to the sequences. Default is 0.
+        - fastaFile2 (str, optional): Path to an additional FASTA file to be concatenated with the first one. Default is None.
+        - fastaFile3 (str, optional): Path to a third FASTA file to be concatenated with the first one. Default is None.
+        """
         self.nc_map, self.nc_map_rev = ncrna_map()
         self.ncdf = read_fasta_file(fastaFile)
         if fastaFile2 is not None:
@@ -125,9 +187,13 @@ class NcRnaDataset(Dataset):
         self.ncdf['real_sequence_length'] = self.ncdf['sequence'].apply(len)
         self.seq_length = seq_length
         self.random_rev_compl_transform_prob = random_rev_compl_transform_prob
+
+     #*********************************************
         
     def __len__(self):
         return len(self.ncdf)
+
+    #*********************************************
 
     def __getitem__(self, idx):
         ncrna_seq = self.ncdf.iloc[idx]['sequence']
@@ -138,57 +204,28 @@ class NcRnaDataset(Dataset):
         if self.seq_length !=0:
             ncrna_onehot_a_seq= self.resize_seqncr(ncrna_onehot_a_seq)
         return ncrna_onehot_a_seq, label 
+
+    #*********************************************
     
     def resize_seqncr(self,nconehot):
-        # Truncate or pad the sequence to the fixed length (max_length)
         seq_len = nconehot.shape[0]
-        
-        # Truncate if longer than seq_length
         if seq_len > self.seq_length:
-            nconehot = nconehot[:self.seq_length, :]  # Truncate to seq_length
-        
-        # Pad if shorter than max_length
+            nconehot = nconehot[:self.seq_length, :]  
         elif seq_len < self.seq_length:
             padding = self.seq_length - seq_len
             nconehot = F.pad(nconehot, (0, 0, 0, padding), value=0)  
-
         return nconehot
 
+    #*********************************************
+    
     def hist_nc_class(self):
         return hist_plot_ncrna_class(self.ncdf)
 
+    #*********************************************
+    
     def dist_nc_len(self):
         return dist_seq_lenghts(self.ncdf)
 
-
-#****************************************************
-
-
-# The idea here was to dynamically adjust the size by batch, but the implementation becomes complex. 
-# At least in the base class definition of `run`, it is not used. 
-# Instead, the sequences are truncated or padded according to a sequence length ajustement (seq_lenght_ajust).
-
-def collate_fn(batch):
-    """
-    Custom collate function to pad one-hot encoded DNA sequences.
-    
-    Args:
-        batch (list of tuples or tensors): Each tuple may contain (sequence, label).
-    
-    Returns:
-        torch.Tensor: Padded batch of one-hot encoded sequences.
-        torch.Tensor: Sequence lengths before padding.
-    """
-    print(type(batch))
-    iseq, ys = zip(*batch)
-    maxlength = max([t.shape[0] for t in iseq])
-    niseq= []
-    for t in iseq:
-        iseqcom = torch.zeros(maxlength-t.shape[0],4)
-        t = torch.vstack((t, iseqcom))
-        niseq.append(t)
-   
-    return list(zip(niseq,ys))
-#******************************************************
+    #*********************************************
 
 
--- modelsnc/modelclasses.py
+++ modelsnc/modelclasses.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 # definition of the models
 
-# A very simple linear model to test
+# A very simple linear model 
 # Note: an extra class was added in case of unknown ncRNA class
 
 class SimpleLinear(nn.Module):
@@ -23,6 +23,10 @@ class SimpleLinear(nn.Module):
 # A simple MLP
 
 class SimpleMLP(nn.Module):
+    """
+    A simple Multi-Layer Perceptron (MLP) model for the classification of non-coding RNAs.
+    """    
+    
     def __init__(self, input_size=10, hidden_size=100, num_classes=14):
         super().__init__()
         self.num_classes = num_classes 
@@ -43,6 +47,11 @@ class SimpleMLP(nn.Module):
 #*********************************************
 # 
 class CombineCNN1(nn.Module):
+    """
+     A Convolutional Neural Network (CNN) model for the classification of non-coding RNAs.
+     This model uses 3 convolutional layers to extract features from the input ncRNA sequences.
+        
+    """    
     def __init__(self, input_size=10, input_channels=4, num_classes=14,dropout_prob=0.5):
         super().__init__()
         self.num_classes = num_classes 
@@ -74,7 +83,12 @@ class CombineCNN1(nn.Module):
 
 #********************************************
 #
-class rnnLSTM(nn.Module):
+class RnnLSTM(nn.Module):
+     """
+      An LSTM-based model for the classification of non-coding RNAs.
+      This model uses a Long Short-Term Memory (LSTM) network to capture sequential dependencies in ncRNA sequences. 
+    """    
+    
     def __init__(self, input_size=10, hidden_size=128, num_classes=14):
         super().__init__()
         self.hidden_size = hidden_size
@@ -98,5 +112,54 @@ class rnnLSTM(nn.Module):
         return "rnn_LSTM"
 
 #********************************************
+# 
+class PositionalEncoding(nn.Module):
+    """
+     Auxiliary class to produce positional encodings used with transformer models.
+     This class generates positional encodings using sine and cosine functions.
+     The implementation is based on the COMP6841 Lab 7 course material.
+    """    
+    def __init__(self, embed_size, max_len=500):
+       
+        super().__init__()
+        pe = torch.zeros(max_len, embed_size)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term  = 10000 ** (-torch.arange(0, embed_size, 2).float() / embed_size) 
+        pe[:, 0::2] = torch.sin(position / div_term)
+        pe[:, 1::2] = torch.cos(position / div_term) 
+        self.pe = pe.unsqueeze(0)
 
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1), :].to(x.device)
+
+
+#********************************************
+# 
+class Transformer(nn.Module):
+    """
+     A Transformer-based model for the classification of non-coding RNAs.
+     The number of input channels is set to 4 to represent the four nucleotide bases.
+    """    
+    def __init__(self, input_size=10, input_channels=4, num_classes=14, d_model=64, nhead=4, dropout=0.1):
+        super().__init__()
+        self.num_classes = num_classes
+        self.embedding = nn.Linear(input_channels, d_model)
+        self.positional_encoding = PositionalEncoding(embed_size=d_model, max_len=input_size)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout, batch_first=True)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=1)
+        self.fc = nn.Linear(d_model * input_size, num_classes)
+
+    def forward(self, x):
+        x = self.embedding(x)  
+        x = self.positional_encoding(x)
+        x = self.encoder(x)  
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def getModelName(self):
+        return "Transformer"
+
+#********************************************
+#
 
